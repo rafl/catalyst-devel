28deeffdcb0f8932bd9730e09c34cdf3aadb0f7c
Marcus Ramberg <marcus@nordaaker.com>
2008-02-05 20:37:11 +0000

    Update default actions in generated root.pm

--- lib/Catalyst/Helper.pm
+++ lib/Catalyst/Helper.pm
@@ -693,11 +693,18 @@ __PACKAGE__->config->{namespace} = '';
 
 =cut
 
-sub default : Private {
+sub index : Path Args(0) {
     my ( $self, $c ) = @_;
 
     # Hello World
     $c->response->body( $c->welcome_message );
+}
+
+sub default : Path {
+    my ( $self, $c ) = @_;
+    $c->response->body( 'Page not found' );
+    $c->response->status(404);
+    
 }
 
 =head2 end
