11d090846da613b5f4fdc5efee5b8cae806cc0dd
Kieren Diment <diment@gmail.com>
2008-05-27 12:01:28 +0000

    fixing some style in the helper

--- lib/Catalyst/Helper.pm
+++ lib/Catalyst/Helper.pm
@@ -693,14 +693,14 @@ __PACKAGE__->config->{namespace} = '';
 
 =cut
 
-sub index : Path Args(0) {
+sub index :Path :Args(0) {
     my ( $self, $c ) = @_;
 
     # Hello World
     $c->response->body( $c->welcome_message );
 }
 
-sub default : Path {
+sub default :Path {
     my ( $self, $c ) = @_;
     $c->response->body( 'Page not found' );
     $c->response->status(404);
