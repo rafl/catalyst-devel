1ea64b027cc39871826973be85d54d6237221e8e
Dave Rolsky <autarch@urth.org>
2009-05-02 02:01:04 +0000

    forgot to re-add Watcher.pm

---
+++ lib/Catalyst/Watcher.pm
@@ -0,0 +1,58 @@
+package Catalyst::Watcher;
+
+use Moose;
+use Moose::Util::TypeConstraints;
+
+use Cwd qw( abs_path );
+use File::Spec;
+use FindBin;
+use namespace::clean -except => 'meta';
+
+has regex => (
+    is      => 'ro',
+    isa     => 'RegexpRef',
+    default => sub { qr/(?:\/|^)(?!\.\#).+(?:\.yml$|\.yaml$|\.conf|\.pm)$/ },
+);
+
+my $dir = subtype
+       as 'Str'
+    => where { -d $_ }
+    => message { "$_ is not a valid directory" };
+
+my $array_of_dirs = subtype
+       as 'ArrayRef[Str]',
+    => where { map { -d } @{$_} }
+    => message { "@{$_} is not a list of valid directories" };
+
+coerce $array_of_dirs
+    => from $dir
+    => via { [ $_ ] };
+
+has directories => (
+    is      => 'ro',
+    isa     => $array_of_dirs,
+    default => sub { [ abs_path( File::Spec->catdir( $FindBin::Bin, '..' ) ) ] },
+    coerce  => 1,
+);
+
+has follow_symlinks => (
+    is      => 'ro',
+    isa     => 'Bool',
+    default => 0,
+);
+
+sub instantiate_subclass {
+    my $class = shift;
+
+    if ( eval { require Catalyst::Watcher::Inotify; 1; } ) {
+        return Catalyst::Watcher::Inotify->new(@_);
+    }
+    else {
+        require Catalyst::Watcher::FileModified;
+        return Catalyst::Watcher::FileModified->new(@_);
+    }
+}
+
+__PACKAGE__->meta->make_immutable;
+
+1;
