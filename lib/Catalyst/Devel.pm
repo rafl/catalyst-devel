0e3abe46403e2f1864f1164e89281dee21b3afc7
Florian Ragwitz <rafl@debian.org>
2009-06-10 17:03:17 +0000

    Version 1.18.

--- lib/Catalyst/Devel.pm
+++ lib/Catalyst/Devel.pm
@@ -3,7 +3,7 @@ package Catalyst::Devel;
 use strict;
 use warnings;
 
-our $VERSION             = '1.17';
+our $VERSION             = '1.18';
 our $CATALYST_SCRIPT_GEN = 39;
 
 $VERSION = eval $VERSION;
