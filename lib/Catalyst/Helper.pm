608f6b92c3cc888724102f73290da3c0a493c899
Jesse Sheidlower <jester@panix.com>
2009-07-29 19:58:23 +0000

    POD tweak (Oleg Kostyuk)

--- lib/Catalyst/Helper.pm
+++ lib/Catalyst/Helper.pm
@@ -604,7 +604,7 @@ Create the main application skeleton. This is called by L<catalyst.pl>.
 This method is called by L<create.pl> to make new components
 for your application.
 
-=head3 mk_dir ($path)
+=head2 mk_dir ($path)
 
 Surprisingly, this function makes a directory.
 
