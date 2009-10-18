894bac703ed2ef10835ae924fafeda80915d3594
Tomas Doran <bobtfish@bobtfish.net>
2009-10-18 17:35:51 +0000

    Bump versions

--- lib/Catalyst/Devel.pm
+++ lib/Catalyst/Devel.pm
@@ -3,7 +3,7 @@ package Catalyst::Devel;
 use strict;
 use warnings;
 
-our $VERSION             = '1.20';
+our $VERSION             = '1.21';
 our $CATALYST_SCRIPT_GEN = 39;
 
 $VERSION = eval $VERSION;
