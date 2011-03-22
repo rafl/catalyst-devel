9b2baa9d780770092d8009bba8d1df090bee4fdd
Tomas Doran <bobtfish@bobtfish.net>
2011-03-22 09:18:18 +0000

    Version 1.32

--- lib/Catalyst/Devel.pm
+++ lib/Catalyst/Devel.pm
@@ -4,7 +4,7 @@ use strict;
 use warnings;
 
 # Change Catalyst::Helper also.
-our $VERSION             = '1.31';
+our $VERSION             = '1.32';
 our $CATALYST_SCRIPT_GEN = 40;
 
 $VERSION = eval $VERSION;
--- lib/Catalyst/Helper.pm
+++ lib/Catalyst/Helper.pm
@@ -19,7 +19,7 @@ use namespace::autoclean;
 with 'MooseX::Emulate::Class::Accessor::Fast';
 
 # Change Catalyst/Devel.pm also
-our $VERSION = '1.31';
+our $VERSION = '1.32';
 
 my %cache;
 
